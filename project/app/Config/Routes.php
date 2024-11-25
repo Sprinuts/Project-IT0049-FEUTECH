c3d5a1824cb00c66936a545b314682e957e29f14
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-11-26 00:23:12 +0800

    setup complete blank canvas

--- project/app/Config/Routes.php
+++ project/app/Config/Routes.php
@@ -8,20 +8,5 @@ use CodeIgniter\Router\RouteCollection;
 //$routes->get('/', 'Home::index');
 
 // default routings
-$routes->get('/', 'Operations::index');
-$routes->get("(?i)operations", 'Operations::index');
-$routes->get("(?i)Operations/index", "Operations::index");
-$routes->get("(?i)index", "Operations::index");
-
-// displayInfo routings
-$routes->get("(?i)operations/displayInfo", "Operations::displayInfo");
-$routes->get("(?i)operations/displayInfo/(:num)", "Operations::displayInfo/$1");
-$routes->get("(?i)operations/displayInfo/(:num)/(:segment)", "Operations::displayInfo/$1/$2");
-$routes->get("(?i)operations/displayInfo/(:num)/(:segment)/(:segment)", "Operations::displayInfo/$1/$2/$3");
-$routes->get("(?i)operations/displayInfo/(:num)/(:segment)/(:segment)/(:segment)", "Operations::displayInfo/$1/$2/$3/$4");
-$routes->get("(?i)Operations/displayInfo/(:num)/(:segment)/(:segment)/(:segment)/(:segment)", "Operations::displayInfo/$1/$2/$3/$4/$5");
-
-// compute routings
-$routes->get("(?i)operations/compute", "Operations::compute");
-$routes->get("(?i)operations/compute/(:num)", "Operations::compute/$1");
-$routes->get("(?i)operations/compute/(:num)/(:num)", "Operations::compute/$1/$2");
+$routes->get('/', 'Index::index');
+$routes->get('index', 'Index::index');
---
+++ project/app/Controllers/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class Index extends BaseController{
+    public function index(){
+        echo "hello gawin mo na project";
+    }
+}
+
+
+?>
