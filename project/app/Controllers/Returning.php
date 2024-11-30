29d74b62ce5786bd3546328e31137f9f20026461
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-12-01 01:41:33 +0800

    changes made to returning email

--- project/app/Controllers/Returning.php
+++ project/app/Controllers/Returning.php
@@ -36,13 +36,29 @@ class Returning extends BaseController{
     public function returning($id){
 
         $equipmentsmodel = model('Equipments_model');
+        $usersmodel = model('Users_model');
+
+        $equipment = $equipmentsmodel->where('id', $id)->first();
+
+        $registerdata = $usersmodel->where('username', session()->get('username'))->first();
+        
+        $accessories = ucwords(str_replace('-', ' ', $equipment['accessories']));
 
         $data = [
             'borrower' => null,
-            'borroweddate' => null,
+            'dateborrowed' => null,
         ];
 
         $equipmentsmodel->update($id, $data);
+
+        $email = service('email');
+        $email->setTo($registerdata['email']);
+        $message = "Hello, " . $registerdata['name'] . "!\n\nYou Returned. '".$equipment['equipmentname']."' with "
+        .$accessories.".\n\n<b>From Far Eastern University Institute of Technology</b>";
+        $email->setMessage($message);
+        if(!$email->send()){
+            print_r($email->printDebugger());
+        }
 
         return redirect()->to('returning');
 
