b7b173c74ba4d48aebf98a402be47621a65e29da
Sprinuts <72307963+Sprinuts@users.noreply.github.com>
2024-11-30 12:42:30 +0800

    fix navbar

--- project/app/Views/include/navbar_associate.php
+++ project/app/Views/include/navbar_associate.php
@@ -2,7 +2,7 @@
 
 <nav class="navbar navbar-expand-lg bg-body-primary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="">
+        <a class="navbar-brand" href="<?= base_url(relativePath: 'welcomestudent'); ?>">
             <img src="<?= base_url("public/style/feutech.png") ?>" alt="home" height="50">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
