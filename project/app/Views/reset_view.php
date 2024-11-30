faac993c6eeddd44d74b004724311395228c560e
joshvalerio <joshvalerio420@gmail.com>
2024-11-30 16:49:52 +0800

    reset password design, login password view toggle

--- project/app/Views/reset_view.php
+++ project/app/Views/reset_view.php
@@ -1,16 +1,30 @@
+<head>
+    <link rel="stylesheet" href="<?= base_url('public/style/reset_view.css'); ?>">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
+</head>
+
 <div class="container" id="container">
     <div class="form-container resetpass"></div>
-        <?php if(validation_errors() != null): ?>
-            <div class="alert alert-danger">
-                <p><?= validation_list_errors(); ?></p>
-            </div>
-        <?php endif ?>
-            <form action="<?= base_url('reset/'.esc($resetcode));  ?>" class="resetpass-form" method="POST" id="resetpass-form">
-                <h1>Reset Password</h1>
-                <input type="password" name="newpass" id="newpass" placeholder="New Password">
-                <input type="password" name="confirmpass" id="confirmpass" placeholder="Confirm Password">
-                <button type="submit" id="resetpass-btn">Reset Password</button>
-            </form>
+    <?php if (validation_errors() != null): ?>
+        <div class="alert alert-danger">
+            <p><?= validation_list_errors(); ?></p>
+        </div>
+    <?php endif ?>
+    <form action="<?= base_url('reset/'.esc($resetcode)); ?>" class="resetpass-form" method="POST" id="resetpass-form">
+        <h1>Reset Password</h1>
+
+        <div class="password-container">
+            <input type="password" name="newpass" id="newpass" placeholder="New Password">
+            <i id="eyeNew" class="fa-solid fa-eye-slash toggle-icon" onclick="togglePassword('newpass', 'eyeNew')"></i>
         </div>
 
-<script src="<?= base_url("public/javascript/login_js.js")?>"></script>
+        <div class="password-container">
+            <input type="password" name="confirmpass" id="confirmpass" placeholder="Confirm Password">
+            <i id="eyeConfirm" class="fa-solid fa-eye-slash toggle-icon" onclick="togglePassword('confirmpass', 'eyeConfirm')"></i>
+        </div>
+
+        <button type="submit" id="resetpass-btn">Reset Password</button>
+    </form>
+</div>
+
+<script src="<?= base_url('public/javascript/login_js.js'); ?>"></script>
