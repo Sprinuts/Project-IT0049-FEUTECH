78fcdf66e7c978d9f203d175617e010076a17e45
joshvalerio <joshvalerio420@gmail.com>
2024-12-01 22:00:25 +0800

    reports design

--- project/app/Views/reports_view.php
+++ project/app/Views/reports_view.php
@@ -1,18 +1,17 @@
 <link rel="stylesheet" href="<?= base_url('public/style/reports.css') ?>">
 
-<div class="reports-container">
-    <?php foreach($reports as $report): ?>
-        <div class="report-item alert alert-success">
-            <p class="report-text">
-                <span class="report-username">User <?= $report['username'] ?></span>
-                <span class="report-action"><?= $report['type'] ?></span>
-                <span class="report-equipment"><?= $report['equipmentid'] ?></span>
-                <span class="report-equipmentname"><?= $report['equipmentname'] ?></span>
-                <span class="report-category"><?= ucfirst(str_replace('-', ' ', $report['category'])) ?></span>
-            </p>
-        </div>
-    <?php endforeach; ?>
-</div>
+<div class="container">
+    <div class="reports-container">
+        <?php foreach($reports as $report): ?>
+            <div class="report-item alert alert-success">
+                <p class="report-text">
+                    <span class="report-username"><?= $report['username'] ?></span>
+                    <span class="report-action"><?= $report['type'] ?></span>
+                    <span class="report-equipment"><?= $report['equipmentid'] ?></span>
+                </p>
+            </div>
+        <?php endforeach; ?>
+    </div>
 
     <div class="pagination">
         <?= $pager->links(); ?>
